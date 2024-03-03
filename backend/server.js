b3c325d4a6253b83f3ae477ffe01b69879449631
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-03-03 20:17:13 -0300

    FEAT: api render changes

--- backend/server.js
+++ backend/server.js
@@ -3,4 +3,10 @@ require("dotenv").config();
 const app = require("./app");
 const port = process.env.POSTGRES_PORT || 8000;
 
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome to the LexArtLabs Test API! by: @wagnerlopesbr (LinkedIn/GitHub)"
+  );
+});
+
 app.listen(port, () => console.log(`Server on port ${port}`));
