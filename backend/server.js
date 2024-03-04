aff9b82d74e4d6cf25d2e075292d75f6dc46e806
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-03-03 22:00:44 -0300

    FEAT: vercel pre deploy changes v5

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,10 @@ const app = require("./app");
 const port = process.env.POSTGRES_PORT || 8000;
 
 app.get("/", (req, res) => {
-  res.send(
-    "Welcome to the LexArtLabs Test API! by: @wagnerlopesbr (LinkedIn/GitHub)"
-  );
+  res.json({
+    message:
+      "Welcome to the LexArtLabs Test API! by: @wagnerlopesbr (LinkedIn/GitHub)",
+  });
 });
 
 app.listen(port, () => console.log(`Server on port ${port}`));
