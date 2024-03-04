ddd75cd61a5262679dc23ad32d3992d90fb78101
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-03-04 13:05:21 -0300

    related to vercel deploy

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,8 @@
-// require("dotenv").config();
-// const express = require("express");
-// const app = require("./app");
-
-// const port = process.env.POSTGRES_PORT || 8000;
-
-// app.get("/", (req, res) => {
-//   res.send(
-//     "Welcome to the LexartLabs Test API! by: @wagnerlopesbr (LinkedIn/GitHub)"
-//   );
-// });
-
-// app.listen(port, () => console.log(`Server on port ${port}`));
-
+require("dotenv").config();
 const express = require("express");
-const app = express();
+const app = require("./app");
+
+const port = process.env.POSTGRES_PORT || 8000;
 
 app.get("/", (req, res) => {
   res.send(
@@ -21,6 +10,4 @@ app.get("/", (req, res) => {
   );
 });
 
-app.listen(8000, () => console.log(`Server on port 8000`));
-
-module.exports = app;
+app.listen(port, () => console.log(`Server on port ${port}`));
