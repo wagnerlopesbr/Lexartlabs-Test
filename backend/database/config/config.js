5246a193e8a1b6e9a91863fdbe68fe37f99ee510
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-03-04 17:59:01 -0300

    vercel deploy ?

--- backend/database/config/config.js
+++ backend/database/config/config.js
@@ -2,7 +2,21 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    database: process.env.POSTGRES_DATABASE || "verceldb",
+    database: process.env.POSTGRES_DATABASE,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    dialect: "postgres",
+    port: 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+  production: {
+    database: process.env.POSTGRES_DATABASE,
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     host: process.env.POSTGRES_HOST,
