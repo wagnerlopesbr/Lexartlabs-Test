e69dfd2e85e29960a9377b02594eb941ec510e11
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-03-04 17:42:49 -0300

    vercel deploy ?

--- backend/database/models/index.js
+++ backend/database/models/index.js
@@ -11,7 +11,12 @@ let sequelize;
 if (config && config.url) {
   sequelize = new Sequelize(config.url, config);
 } else {
-  sequelize = new Sequelize(config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
 fs.readdirSync(__dirname)
